e36ec73736900e881c9fdc31e6e6d6d9bb240ae9
Christian Liebhardt <christian.liebhardt@arcor.de>
2019-04-22 21:57:04 +0200

    Added support for widening primitive conversions

--- src/main/java/com/github/liebharc/queryenrichment/IntermediateResult.java
+++ src/main/java/com/github/liebharc/queryenrichment/IntermediateResult.java
@@ -16,14 +16,51 @@ public class IntermediateResult {
         results.put(selector.getAttribute(), result);
     }
 
+    @SuppressWarnings("unchecked")
     public<T> T get(Attribute<T> attribute) {
         return (T)results.get(attribute);
     }
 
     public void addFromQuery(Selector<?> selector) {
         // Plan builder ensures that we can rely on the query result position here
-        results.put(selector.getAttribute(), queryResult.get(queryResultPos));
+        Attribute<?> attribute = selector.getAttribute();
+        results.put(attribute, this.cast(attribute.getAttributeClass(), queryResult.get(queryResultPos)));
         this.nextColumn();
+    }
+
+    /**
+     * Cast the value to the given class. Also supports the most crucial widening primitive conversions.
+     */
+    private Object cast(Class<?> clazz, Object value) {
+        if (clazz == Long.class) {
+            if (value instanceof Long) {
+                return value;
+            }
+
+            if (value instanceof Integer) {
+                return (long) ((int) value);
+            }
+
+            if (value instanceof Short) {
+                return (long) ((short) value);
+            }
+        }
+
+        if (clazz == Integer.class) {
+            if (value instanceof Integer) {
+                return value;
+            }
+
+            if (value instanceof Short) {
+                return (int) ((short) value);
+            }
+        }
+
+        if (clazz == Double.class && value instanceof Float) {
+            return (double)((float)value);
+        }
+
+        return clazz.cast(value);
     }
 
     private void nextColumn() {
