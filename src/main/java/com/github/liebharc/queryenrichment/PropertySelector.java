508dfb3d09acb59ff6386bee7bd587bea972ad4d
Christian Liebhardt <christian.liebhardt@arcor.de>
2019-04-27 13:37:51 +0200

    Added PropertySelector

---
+++ src/main/java/com/github/liebharc/queryenrichment/PropertySelector.java
@@ -0,0 +1,11 @@
+package com.github.liebharc.queryenrichment;
+
+/**
+ * A shortcut for a select where the column name matches the property name of the attribute.
+ * @param <T> Attribute type
+ */
+public class PropertySelector<T> extends Selector<T> {
+    public PropertySelector(Attribute<T> attribute) {
+        super(attribute, attribute.getProperty());
+    }
+}
